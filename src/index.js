fa1bbaa5904724ba50a5d718c80657cac29ec046
Alan Contreras <alanrcontrerasm@gmail.com>
2021-02-13 21:02:42 -0800

    Add structure, config npm, webpack

---
+++ src/index.js
@@ -0,0 +1,18 @@
+"use strict";
+import _ from 'lodash';//optional, must run npm install --save lodash
+import {DisplayController, Player} from './myModule.js'
+
+DisplayController.saySomething("HelloWorld");
+const alan = Player("Alan");
+console.log(alan.getPlayer());
+
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, imported
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
